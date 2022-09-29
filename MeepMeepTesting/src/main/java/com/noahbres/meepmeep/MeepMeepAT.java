e43c374e279a6a2fda2853838cdb24daed76887c
SaiabhiramA <109114640+SaiabhiramA@users.noreply.github.com>
2022-09-28 20:17:02 -0400

    Added MeepMeep AT File with custom paths
    Added custom paths

---
+++ MeepMeepTesting/src/main/java/com/noahbres/meepmeep/MeepMeepAT.java
@@ -0,0 +1,62 @@
+package com.noahbres.meepmeep;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepAT {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                //.addEntity(blueAllianceRightStraightTuns(meepMeep))
+                .addEntity(blueAllianceRightSplaineTuns(meepMeep))
+                .start();
+    }
+    private static RoadRunnerBotEntity blueAllianceRightSplaineTuns(MeepMeep atRobot){
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(atRobot)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(270), Math.toRadians(270), 14)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 60, 0))
+                                //.back(30)
+                                //.turn(Math.toRadians(-90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(45))
+                                //.forward(20)
+                                .splineToLinearHeading(new Pose2d(-52, 10, Math.toRadians(90)), Math.toRadians(0))
+                                //.splineToLinearHeading(new Pose2d(52, 10, Math.toRadians(90)), Math.toRadians(20))
+                                //.strafeLeft(10)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                .build()
+                );
+        return myBot;
+    }
+
+    private static RoadRunnerBotEntity blueAllianceRightStraightTuns(MeepMeep atRobot){
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(atRobot)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 60, 0))
+                                .back(30)
+                                .turn(Math.toRadians(-90))
+                                .forward(30)
+                                .turn(Math.toRadians(45))
+                                .forward(20)
+                                //.strafeLeft(10)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                .build()
+                );
+        return myBot;
+    }
+}
