7d9f566f3796eee6ed9f7dd652f48c52e21ede63
SaiabhiramA <109114640+SaiabhiramA@users.noreply.github.com>
2022-10-02 22:04:08 -0400

    Update MeepMeepAT.java
    Blue Alliance path Design

--- MeepMeepTesting/src/main/java/com/noahbres/meepmeep/MeepMeepAT.java
+++ MeepMeepTesting/src/main/java/com/noahbres/meepmeep/MeepMeepAT.java
@@ -1,5 +1,6 @@
 package com.noahbres.meepmeep;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -19,13 +20,15 @@ public class MeepMeepAT {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(270), Math.toRadians(270), 14)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-35, 60, 0))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 62, -90))
                                 //.back(30)
                                 //.turn(Math.toRadians(-90))
                                 //.forward(30)
                                 //.turn(Math.toRadians(45))
                                 //.forward(20)
-                                .splineToLinearHeading(new Pose2d(-52, 10, Math.toRadians(90)), Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(-60, 57.5), Math.toRadians(-100))
+                                //.splineToLinearHeading(new Pose2d(-62, 30, Math.toRadians(-90)), Math.toRadians(-90))
+                                .splineToLinearHeading(new Pose2d(-50, 12, Math.toRadians(-90)), Math.toRadians(0))
                                 //.splineToLinearHeading(new Pose2d(52, 10, Math.toRadians(90)), Math.toRadians(20))
                                 //.strafeLeft(10)
                                 //.turn(Math.toRadians(90))
